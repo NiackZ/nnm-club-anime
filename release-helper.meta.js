8489fe96d1279560191a6a9a17f82adab80fc057
ElSwanko <elswanko@gmail.com>
2016-04-02 02:52:45 +0300

    Update release-helper.meta.js

--- release-helper.meta.js
+++ release-helper.meta.js
@@ -2,11 +2,11 @@
 // @name          nnm-club^anime releaser helper
 // @namespace     nnm-club^anime.Scripts
 // @description   Генерация оформления релиза по данным на странице аниме в базе World-Art
-// @version       1.0.0.3
+// @version       1.0.0.4
 // @author        ElSwanko
 // @homepage      https://github.com/ElSwanko/nnm-club-anime
 // @updateURL     https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.meta.js
-// @downloadURL   https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.full.js
+// @downloadURL   https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.user.js
 // @include       http://www.world-art.ru/animation/*
 // @include       https://www.world-art.ru/animation/*
 // @match         http://www.world-art.ru/animation/*
