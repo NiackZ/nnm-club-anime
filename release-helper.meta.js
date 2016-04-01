68522e6b82784de006e5d5184bead4a3ef0bff46
ElSwanko <elswanko@gmail.com>
2016-04-01 21:04:11 +0300

    Create release-helper.meta.js

---
+++ release-helper.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          nnm-club^anime releaser helper
+// @namespace     nnm-club^anime.Scripts
+// @description   Генерация оформления релиза по данным на странице аниме в базе World-Art
+// @version       1.0.0.1
+// @author        ElSwanko
+// @homepage      https://github.com/ElSwanko/nnm-club-anime
+// @updateURL     https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.meta.js
+// @downloadURL   https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.full.js
+// @include       http://www.world-art.ru/animation/*
+// @include       https://www.world-art.ru/animation/*
+// @match         http://www.world-art.ru/animation/*
+// @match         https://www.world-art.ru/animation/*
+// @include       http://nnmclub.to/forum/release.php?what=anime_common*
+// @include       https://nnmclub.to/forum/release.php?what=anime_common*
+// @include       http://*.nnmclub.to/forum/release.php?what=anime_common*
+// @include       https://*.nnmclub.to/forum/release.php?what=anime_common*
+// @match         http://nnmclub.to/forum/release.php?what=anime_common*
+// @match         https://nnmclub.to/forum/release.php?what=anime_common*
+// @match         http://*.nnmclub.to/forum/release.php?what=anime_common*
+// @match         https://*.nnmclub.to/forum/release.php?what=anime_common*
+// @grant         none
+// ==/UserScript==
+//
