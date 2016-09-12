5ec1acb689907018c883f70c5ae446b5831557b5
ElSwanko <elswanko@gmail.com>
2016-09-12 13:55:52 +0300

    Update release-helper.meta.js
    version increment

--- release-helper.meta.js
+++ release-helper.meta.js
@@ -2,7 +2,7 @@
 // @name          nnm-club^anime releaser helper
 // @namespace     nnm-club^anime.Scripts
 // @description   Генерация оформления релиза по данным на странице аниме в базе World-Art
-// @version       1.0.0.18
+// @version       1.0.0.19
 // @author        ElSwanko edited by NIK220V
 // @homepage      https://github.com/ElSwanko/nnm-club-anime
 // @updateURL     https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.meta.js
