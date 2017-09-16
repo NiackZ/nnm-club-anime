d7d9f3db92d9cc12f1b1295bc49f32ff7ce06840
ElSwanko <elswanko@gmail.com>
2017-09-16 23:46:01 +0300

    Increment version

--- release-helper.meta.js
+++ release-helper.meta.js
@@ -2,25 +2,16 @@
 // @name          nnm-club^anime releaser helper
 // @namespace     nnm-club^anime.Scripts
 // @description   Генерация оформления релиза по данным на странице аниме в базе World-Art
-// @version       1.0.0.21
+// @version       1.0.0.22
 // @author        ElSwanko edited by NIK220V
 // @homepage      https://github.com/ElSwanko/nnm-club-anime
 // @updateURL     https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.meta.js
 // @downloadURL   https://github.com/ElSwanko/nnm-club-anime/raw/master/release-helper.user.js
-// @include       http://www.world-art.ru/animation/*
 // @match         http://www.world-art.ru/animation/*
-// @include       http://nnmclub.to/forum/release.php?what=anime_common*
-// @match         http://nnmclub.to/forum/release.php?what=anime_common*
-// @include       http://*.nnmclub.to/forum/release.php?what=anime_common*
-// @match         http://*.nnmclub.to/forum/release.php?what=anime_common*
-// @include       https://nnmclub.to/forum/release.php?what=anime_common*
-// @match         https://nnmclub.to/forum/release.php?what=anime_common*
-// @include       https://*.nnmclub.to/forum/release.php?what=anime_common*
-// @match         https://*.nnmclub.to/forum/release.php?what=anime_common*
-// @include       http://dsenxis5txr4zbxe.onion/forum/release.php?what=anime_common*
-// @match         http://dsenxis5txr4zbxe.onion/forum/release.php?what=anime_common*
-// @include       https://dsenxis5txr4zbxe.onion/forum/release.php?what=anime_common*
-// @match         https://dsenxis5txr4zbxe.onion/forum/release.php?what=anime_common*
+// @match         *://*.nnmclub.to/forum/release.php?what=anime_common*
+// @match         *://*.nnm-club.me/forum/release.php?what=anime_common*
+// @match         *://*.nnm-club.name/forum/release.php?what=anime_common*
+// @match         *://dsenxis5txr4zbxe.onion/forum/release.php?what=anime_common*
 // @grant         none
 // ==/UserScript==
 //
